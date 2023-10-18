6a46fdbf6c0758726b492504deed35a02ec6664e
CapovillaB <brunocapovilla1@gmail.com>
2023-10-18 16:22:34 -0300

    Initial commit

---
+++ src/main/java/com/pizzaria/sis_pedido/SisPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaria.sis_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisPedidoApplication.class, args);
+	}
+
+}

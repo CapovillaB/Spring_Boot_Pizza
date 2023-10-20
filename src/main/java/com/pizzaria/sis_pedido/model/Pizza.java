84922acc5139ccb6c52bc80029caa87f09d8d411
Wyver Dev <wyverc@gmail.com>
2023-10-20 16:28:12 -0300

    adição da superclasse pai "produto" e das filhas bebida e pizza

---
+++ src/main/java/com/pizzaria/sis_pedido/model/Pizza.java
@@ -0,0 +1,32 @@
+package com.pizzaria.sis_pedido.Model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+//   @Table(name = "nome da tabela no banco")    //caso a classe tenha um nome diferente da tabela no banco de dados
+public class Pizza extends Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pizza")
+    private int id;
+    @Column(name = "pz_type")
+    private String tipo; //adicionei pois estava anteriormente mas nao sei qual função em pizzas
+    @Column(name = "pz_size")
+    private String tamanho;
+    @Column(name = "pz_flavor")
+    private String sabor;
+    @Column(name = "pz_desc")
+    private String descricao;
+    @Column(name = "pz_price")
+    private float valor;
+
+    
+}
--- src/main/java/com/pizzaria/sis_pedido/model/Usuario.java
+++ src/main/java/com/pizzaria/sis_pedido/model/Usuario.java
@@ -1,6 +1,11 @@
-package com.pizzaria.sis_pedido.model;
+package com.pizzaria.sis_pedido.Model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
